243d4ad3c1a82135a565d97bf845e256ebeb3c81
Spartacus <k_morter@outlook.com>
2020-09-17 10:44:27 -0600

    added typeDefs from challenge instructions

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,8 +1,19 @@
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
-      }
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user.id })
+                .select('-__v -password')
+                .populate('savedBooks');
+
+                return userData
+            }
+            throw new AuthenticationError('Not logged in');
+        }
     }
   };
   
--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -3,8 +3,37 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: INT
+        savedBooks: [Book]
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
     type Query {
-        helloWorld: String
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        savedBook(input: bookInput): User
+        removeBook:(bookId: String!): User
     }
 `;
 
