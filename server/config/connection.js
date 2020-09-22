8c286b4d8fa7442c0dc9fa4d6332b239c6b2fff3
Spartacus <k_morter@outlook.com>
2020-09-22 12:12:07 -0600

    added mongoDB connneciton for heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', 
+MONGODB_URI = `mongodb+srv://kaileymorter:${process.env.DB_PASS}@cluster0.hq8r9.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+mongoose.connect(MONGODB_URI,
     {
     useNewUrlParser: true,
     useUnifiedTopology: true,
